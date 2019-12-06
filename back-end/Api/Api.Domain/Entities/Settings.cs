116b4871216948f1a24ac50a9e847f4110088174
Evgeni Manolov <evg_manolov@abv.bg>
2019-12-06 22:40:09 +0200

    Created db structure of models/entities.

---
+++ back-end/Api/Api.Domain/Entities/Settings.cs
@@ -0,0 +1,13 @@
+﻿namespace Api.Domain.Entities
+{
+    public class Settings
+    {
+        public string Id { get; set; }
+
+        public bool ShowOutOfStock { get; set; }
+
+        public string Name { get; set; }
+
+        public int Value { get; set; }
+    }
+}
