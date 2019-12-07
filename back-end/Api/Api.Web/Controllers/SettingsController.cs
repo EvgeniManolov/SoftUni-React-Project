b10f0e82b63a37ec1f5e593d155d1dbeccd60dbd
Evgeni Manolov <evg_manolov@abv.bg>
2019-12-08 00:28:10 +0200

    Fixes some bugs and add news.

---
+++ back-end/Api/Api.Web/Controllers/SettingsController.cs
@@ -0,0 +1,48 @@
+﻿namespace Api.Web.Controllers
+{
+    using Api.Models.Settings;
+    using Api.Services.Interfaces;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class SettingsController : BaseController
+    {
+        private readonly ISettingsService settings;
+        public SettingsController(ISettingsService settings, IUserService users) : base(users, settings)
+        {
+            this.settings = settings;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+  
+            return await this.Execute(isAdmin: false, checkState: false, function: async () =>
+            {
+                SettingsViewEditModel settings = await this.settings.Get();
+
+                return this.Ok(settings.Settings);
+            });
+        }
+
+        [HttpPut]
+        //[Authorize]
+        public async Task<IActionResult> Update([FromQuery]IDictionary<string, int> data)
+        {
+
+            return await this.Execute(isAdmin: false, checkState: false, function: async () =>
+
+            {
+                await this.settings.Update(new SettingsViewEditModel() { Settings = data});
+
+                SettingsViewEditModel settings = await this.settings.Get();
+
+                return this.Ok(settings.Settings);
+            });
+        }
+    }
+}
