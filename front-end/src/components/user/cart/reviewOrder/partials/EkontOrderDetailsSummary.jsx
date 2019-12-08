c40e110595f016c87cbda6252a912086c3c34a2c
Evgeni Manolov <evg_manolov@abv.bg>
2019-12-08 14:32:31 +0200

    Create order models, services, controllers.

---
+++ front-end/src/components/user/cart/reviewOrder/partials/EkontOrderDetailsSummary.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { CART } from '../../../../../data/constants/componentConstants';
+
+class EkontOrderDetailsSummary extends React.Component {
+	constructor (props) {
+		super(props);
+	}
+
+	render () {
+		let orderDetails = this.props.deliveryDetails;
+
+		return (
+			<div>
+				<h4>{CART.toEkontOffice}</h4>
+				<p><strong>&#8470;&nbsp;{orderDetails.officeCode}</strong> - {orderDetails.officeName}</p>
+
+				<p>{orderDetails.country}</p>
+				<p>{orderDetails.address}</p>
+			</div>
+		);
+	}
+}
+
+export default EkontOrderDetailsSummary;
